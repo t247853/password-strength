9ec0342d0d869b7177de6f5da122f2d1c47f50be
Olun4ik <olaharlap@MacBook-Pro.local>
2024-11-05 16:12:08 +0200

    Revert "added dist"
    This reverts commit 5cb0701e6cfc0e50f9e171bac23bcd1d2ea44791.

--- dist/angular-material-extensions/password-strength/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@angular-material-extensions/password-strength" />
-export * from './public-api';
--- dist/angular-material-extensions/password-strength/lib/component/mat-pass-toggle-visibility/mat-pass-toggle-visibility.component.d.ts
+++
@@ -1,8 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MatPassToggleVisibilityComponent {
-    isVisible: boolean;
-    tabindex?: string;
-    get type(): "text" | "password";
-    static ɵfac: i0.ɵɵFactoryDeclaration<MatPassToggleVisibilityComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MatPassToggleVisibilityComponent, "mat-pass-toggle-visibility", never, { "isVisible": { "alias": "isVisible"; "required": false; }; "tabindex": { "alias": "tabindex"; "required": false; }; }, {}, never, never, false, never>;
-}
